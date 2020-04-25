2da97b9d838a555c09a93f3d86a08409ffaae2fa
Gilbert Gamboa <gilbert.g.alvarado@gmail.com>
2020-04-24 19:29:50 -0600

    other1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const db = require('./server/models/db');
 var cors = require('cors');
 const app = express();
-const PORT = 3000; 
+const PORT = process.env.PORT || 3000; 
 const seed = require('./server/models/seed/seed-bd');
 
 // setup the Express middleware
